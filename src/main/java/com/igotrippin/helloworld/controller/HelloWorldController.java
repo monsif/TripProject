f7009c361b91b7fd1eed62ec0cefccfaeaacdaf7
Monsif <mabrouk.monsif@gmail.com>
2016-04-19 02:45:52 +0200

    spring mvc with maven project hello world

---
+++ src/main/java/com/igotrippin/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.igotrippin.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+	String message = "Welcome to Spring MVC!";
+ 
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller"+name);
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/helloworld.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Spring 4 MVC -HelloWorld</title>
+</head>
+<body>
+		<h2>Hello World</h2>
+		<h2>
+			${message} ${name}
+		</h2>
+</body>
+</html>
